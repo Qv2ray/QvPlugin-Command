c76e9e355377310ac253e1d377080eb04a2dc211
xz-dev <32761048+xz-dev@users.noreply.github.com>
2021-03-13 20:30:02 +0800

    fix: fix config not load

--- CommandPlugin.cpp
+++ CommandPlugin.cpp
@@ -3,8 +3,9 @@
 #include "ui/CommandPluginSettings.hpp"
 #include "ui/GUIInterface.hpp"
 
-bool CommandPlugin::InitializePlugin(const QString &, const QJsonObject &)
+bool CommandPlugin::InitializePlugin(const QString &, const QJsonObject &conf)
 {
+    Qv2rayInterface::UpdateSettings(conf);
     CommandPluginInstance = this;
     eventHandler = std::make_shared<SimpleEventHandler>();
     guiInterface = new CommandGUIInterface();
